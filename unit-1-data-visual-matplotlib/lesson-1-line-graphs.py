d474c18db2d5f92938fe3950db1c84c6ae668fd7
Danni Tang <tangdnn@gmail.com>
2017-12-05 14:06:34 -0500

    checking in initial code

---
+++ unit-1-data-visual-matplotlib/lesson-1-line-graphs.py
@@ -0,0 +1,120 @@
+
+# Basic Line Plot
+
+#import codecademylib
+from matplotlib import pyplot as plt
+
+days = [0, 1, 2, 3, 4, 5, 6]
+money_spent = [10, 12, 12, 10, 14, 22, 24]
+plt.plot(days, money_spent)
+plt.show()
+
+# Basic Line Plot II
+
+time = [0, 1, 2, 3, 4]
+revenue = [200, 400, 650, 800, 850]
+costs = [150, 500, 550, 550, 560]
+plt.plot(time, revenue)
+plt.plot(time, costs)
+plt.show()
+
+# Linestyles
+
+plt.plot(time, revenue, color = 'purple', linestyle = '--')
+plt.plot(time, costs, color = '#82edc9', marker = 's')
+
+# Axis and Labels
+
+x = range(12)
+y = [3000, 3005, 3010, 2900, 2950, 3050, 3000, 3100, 2980, 2980, 2920, 3010]
+plt.plot(x, y)
+
+#your code here
+plt.axis([0, 12, 2900, 3100])
+plt.show()
+
+# Labeling the Axes
+
+x = range(12)
+y = [3000, 3005, 3010, 2900, 2950, 3050, 3000, 3100, 2980, 2980, 2920, 3010]
+plt.plot(x, y)
+plt.axis([0, 12, 2900, 3100])
+plt.xlabel('Time')
+plt.ylabel('Dollars spent on coffee')
+plt.title('My Last Twelve Years of Coffee Drinking')
+plt.show()
+
+# Subplots
+
+months = range(12)
+temperature = [36, 36, 39, 52, 61, 72, 77, 75, 68, 57, 48, 48]
+flights_to_hawaii = [1200, 1300, 1100, 1450, 850, 750, 400, 450, 400, 860, 990, 1000]
+
+plt.subplot(1, 2, 1)
+plt.plot(months, temperature)
+
+plt.subplot(1, 2, 2)
+plt.plot(flights_to_hawaii, temperature, "o")
+
+plt.show()
+
+# Subplots Part II
+
+x = range(7)
+straight_line = [i for i in x]
+parabola = [i**2 for i in x]
+cubic = [i**3 for i in x]
+
+plt.subplot(2, 1, 1)
+plt.plot(x, straight_line)
+
+plt.subplot(2, 2, 3)
+plt.plot(x, parabola)
+
+plt.subplot(2, 2, 4)
+plt.plot(x, cubic)
+
+plt.subplots_adjust(wspace=0.35)
+plt.subplots_adjust(bottom=0.2)
+
+plt.show()
+
+# Legends
+
+months = range(12)
+hyrule = [63, 65, 68, 70, 72, 72, 73, 74, 71, 70, 68, 64]
+kakariko = [52, 52, 53, 68, 73, 74, 74, 76, 71, 62, 58, 54]
+gerudo = [98, 99, 99, 100, 99, 100, 98, 101, 101, 97, 98, 99]
+
+plt.plot(months, hyrule)
+plt.plot(months, kakariko)
+plt.plot(months, gerudo)
+
+#create your legend here
+legend_labels = ["Hyrule", "Kakariko", "Gerudo Valley"]
+plt.legend(legend_labels, loc=9)
+plt.show()
+
+# Modify Ticks
+
+month_names = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep","Oct", "Nov", "Dec"]
+
+months = range(12)
+conversion = [0.05, 0.08, 0.18, 0.28, 0.4, 0.66, 0.74, 0.78, 0.8, 0.81, 0.85, 0.85]
+
+plt.xlabel("Months")
+plt.ylabel("Conversion")
+
+plt.plot(months, conversion)
+
+# Your work here
+ax = plt.subplot()
+ax.set_xticks(months)
+ax.set_xticklabels(month_names)
+ax.set_yticks([0.10, 0.25, 0.5, 0.75])
+ax.set_yticklabels(["10%", "25%", "50%", "75%"])
+
+plt.show()
+
+# Figures
+
